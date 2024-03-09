703ea923d2bde88f9c9fee68c23998b1175dac68
Viktoriia Kostak <viktoriakostak570@gmail.com>
2024-03-09 13:13:26 +0300

    Documentation improvements, authorization design and fix bugs

--- src/apartment/schemas/apartment.schema.ts
+++ src/apartment/schemas/apartment.schema.ts
@@ -1,15 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 export type ApartmentDocument = Apartment & Document;
 
 @Schema()
 export class Apartment {
+  @ApiProperty()
   @Prop()
   id: string;
 
+  @ApiProperty({ required: true })
   @Prop({ required: true })
   name: string;
 
+  @ApiProperty({ required: true })
   @Prop({ required: true })
   isAvailable: boolean;
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { ApartmentModule } from './apartment/apartment.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ApartmentsModule } from './apartments/apartments.module';
+import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,6 +15,9 @@ import { ApartmentsModule } from './apartments/apartments.module';
     MongooseModule.forRoot(process.env.MONGO_URI),
     ApartmentModule,
     ApartmentsModule,
+    AuthModule,
+    UserModule,
   ],
+  providers: [UserService],
 })
 export class AppModule {}
