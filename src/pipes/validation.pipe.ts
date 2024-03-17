913076f13aded68110ba01b95309ba08e7ffe6fe
Viktoriia Kostak <viktoriakostak570@gmail.com>
2024-03-17 23:57:25 +0300

    Init pipes

---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,9 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any>{
+  transform(value: any, metadata: ArgumentMetadata) {
+    return value;
+  }
+}
